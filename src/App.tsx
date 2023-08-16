9a2e95d9c98150a3186d4364a54825d32f7bfc1d
Pedro Tibúrcio <pedrotiburcio10@gmail.com>
2023-08-15 21:32:16 -0300

    commit inicial

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+/*function somar(a,b) {
+  return a+b;
+} */
+
 function App() {
   return (
     <div className="App">
